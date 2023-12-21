ba73e31a3f3b0d2fbef66e4e50b3636d9d1d27ac
Brian Erickson <beric21@wgu.edu>
2023-12-20 17:04:06 -0800

    adding in cart, cartitems calsses

---
+++ src/main/java/com/wgu/d288/entities/Cart.java
@@ -0,0 +1,90 @@
+package com.wgu.d288.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+public class Cart {
+
+    /*
+    * this is the ID for the table
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="cart_id")
+    private Long id;
+
+    /*
+    Strings for the table
+     */
+    /*
+    number to track the order
+     */
+    @Column(name="order_tracking number")
+    private String orderTrackingNumber;
+
+    /*
+    Big decimals for the table
+     */
+    /*
+    price of the package
+     */
+    @Column(name="order_tracking_number")
+    private BigDecimal package_price;
+
+    /*
+    int fields for the table
+     */
+    /*
+    number of people in the party
+     */
+    @Column(name="party_size")
+    private int party_size;
+
+    /*
+    enums of the table
+     */
+    /*
+    status of the order
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(name="status")
+    private StatusType status;
+
+    /*
+    date and datetime fields
+     */
+    /*
+    create_date field.  dateTime field
+     */
+    @CreationTimestamp
+    @Column(name="create_date")
+    private Date create_date;
+
+    /*
+    this is the datetime the record was updated
+     */
+    @UpdateTimestamp
+    @Column(name="last_update")
+    private Date last_update;
+
+    /*
+    relationships to this table
+     */
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
+    private Set<CartItem> cartItem = new HashSet<>();
+
+    public void add(CartItem cartItem){
+        this.cartItem.add(cartItem);
+    }
+}
---
+++ src/main/java/com/wgu/d288/entities/CartItem.java
@@ -0,0 +1,66 @@
+package com.wgu.d288.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+
+/*
+This class holds all the individual items in the cart
+ */
+@Entity
+@Table(name="cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    /*
+    ID for the table
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="cart_item_id")
+    private Long id;
+
+    /*
+    joins to other tables
+     */
+    /*
+    join to the excursions table
+     */
+    @ManyToMany
+    @JoinTable(
+            name="excursion_cartitem",
+            joinColumns = @JoinColumn(name="cart_item_id"),
+            inverseJoinColumns = @JoinColumn(name="excursion_id")
+            )
+    private Set<Excursion> excursions;
+
+    /*
+    join to the cart table
+     */
+    @ManyToOne
+    @JoinColumn(name="cart_id")
+    private Cart cart;
+
+    /*
+    datetime of the table
+     */
+    /*
+    creation datetime of the table
+     */
+    @CreationTimestamp
+    @Column(name="crete_date")
+    private Date create_date;
+
+    /*
+    update time of the table
+     */
+    @UpdateTimestamp
+    @Column(name="last_update")
+    private Date last_update;
+}
