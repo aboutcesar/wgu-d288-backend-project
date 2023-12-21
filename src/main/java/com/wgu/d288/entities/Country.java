42900e257c4a0595017bc7132a679f8b6babc5c6
Brian Erickson <beric21@wgu.edu>
2023-12-20 17:14:29 -0800

    adding in Country class

---
+++ src/main/java/com/wgu/d288/entities/Country.java
@@ -0,0 +1,56 @@
+package com.wgu.d288.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+/*
+this is Country table
+ */
+
+@Entity
+@Table(name ="countries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Country {
+    /*
+    ID of the table
+    @Long
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="country_id")
+    private Long id;
+
+    /*
+    name of the country
+    @String
+     */
+    @Column(name="country")
+    private String country_name;
+
+    /*
+    create date of the table
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="create_date")
+    private Date create_date;
+
+    /*
+    update time stamp for the table
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="last_update")
+    private Date last_update;
+
+    /*
+    mapping to other tables
+     */
+    @OneToMany(mappedBy = "country", cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    private Set<Division> divisions;
+
+}
