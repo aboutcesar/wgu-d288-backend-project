79a57152f7445a1505625f67ea1f339c0207b6a2
Brian Erickson <beric21@wgu.edu>
2023-12-26 07:08:16 -0800

    adding in the Purchase class

---
+++ src/main/java/com/wgu/d288/services/Purchase.java
@@ -0,0 +1,17 @@
+package com.wgu.d288.services;
+
+import com.wgu.d288.entities.CartItem;
+import com.wgu.d288.entities.Cart;
+import com.wgu.d288.entities.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class Purchase {
+    private Cart cart;
+    private Set<CartItem> cartItem;
+    private Customer customer;
+}
